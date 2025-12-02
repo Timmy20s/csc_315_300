c2ba8cf67237f3574e7edd235db0fddb763c0c4d
Timmy20s <190116654+Timmy20s@users.noreply.github.com>
2025-12-02 12:24:51 +0100

    Commit this repo

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'register.dart';
+import 'forgot_password.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,56 +10,82 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
   int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+      child: Scaffold(
         appBar: AppBar(
-          title: Text('CSC309', style: TextStyle(),),
+          title: const Text('CSC309'),
           backgroundColor: Colors.redAccent,
         ),
-        body: Padding(padding: EdgeInsets.all(25),
+        body: Padding(
+          padding: const EdgeInsets.all(25),
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-          TextField(
-          decoration: InputDecoration(
-          hintText: 'Email'
-          ),
-        ),
-        SizedBox(height: 15,),
-        TextField(
-          obscureText: true,
-          obscuringCharacter: '*',
-          decoration: InputDecoration(
-              hintText: 'Password'
-          ),
-        ),
-        SizedBox(height: 15,),
-        InkWell(
-          onTap:(){
-            Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> Login(),),
-            );
-          },
-          child: Text('Forgot Password', style: TextStyle(fontSize: 20, color: Colors.green),)),
-      SizedBox(height: 15,),
-      Text('COUNTING>>> $_counter', style: TextStyle(fontSize: 30)),
-      ElevatedButton(onPressed: (){
-        setState(() {
-          _counter = _counter + 1;
-        });
-        _counter = _counter + 1;
-        debugPrint('COUNTING>>> $_counter', );
-      }, child: Text('Login')),
-      SizedBox(height: 15,),
-      Text('Register', style: TextStyle(fontSize: 20, color: Colors.green),),
-      ],
-    ),
-    //floatingActionButton: FloatingActionButton(onPressed: (){}),
-    )
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: const InputDecoration(hintText: 'Email'),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                obscureText: true,
+                obscuringCharacter: '*',
+                decoration: const InputDecoration(hintText: 'Password'),
+              ),
+              const SizedBox(height: 15),
 
-    ));
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const ForgotPassword()),
+                  );
+                },
+                child: const Text(
+                  'Forgot Password',
+                  style: TextStyle(fontSize: 20, color: Colors.green),
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              Text(
+                'COUNTING>>> $_counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _counter = _counter + 1;
+                  });
+                  debugPrint('COUNTING>>> $_counter');
+                },
+                child: const Text('Login'),
+              ),
+
+              const SizedBox(height: 25),
+
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const Register(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Register',
+                  style: TextStyle(fontSize: 20, color: Colors.green),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
